892dd145ac6e5696c7c370a42053ec1d062996da
Niko Dittmann <mail+git@niko-dittmann.com>
2011-06-05 20:23:40 +0200

    + all fields

--- app.rb
+++ app.rb
@@ -3,14 +3,29 @@ require 'mm_geoip.rb'
 require 'rack_mm_geoip.rb'
 
 class App < Sinatra::Base
+  enable :inline_templates
   use Rack::MMGeoip
   
+  helpers do
+    def geoip(field)
+      request.env['GEOIP'].send field
+    end
+  end
+  
   get '/' do
-    %Q{
-      ip: #{request.env['GEOIP'].ip},
-      city: #{request.env['GEOIP'].city},
-      country_name: #{request.env['GEOIP'].country_name}
-    }
+    haml :index
   end
 end
+
+__END__
+
+@@ index
+%html
+  %head
+    %title mm_geoip with Sinatra
+  %body
+    %ul
+      - (MMGeoip::FIELDS + [:region_name]).each do |field|
+        %li= "#{field}: #{geoip field}"
+
 
