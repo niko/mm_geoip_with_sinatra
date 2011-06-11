9e340e3dcc301b35b7b47321d71d9f045fe911d3
Niko Dittmann <mail+git@niko-dittmann.com>
2011-06-11 15:06:56 +0200

    + now it's a classic style Sinatra app

--- app.rb
+++ app.rb
@@ -1,20 +1,14 @@
-require 'sinatra/base'
-require 'mm_geoip.rb'
-require 'rack_mm_geoip.rb'
+require 'sinatra'
+require 'sinatra/mm_geoip.rb'
 
-class App < Sinatra::Base
-  enable :inline_templates
-  use Rack::MMGeoip
-  
-  helpers do
-    def geoip(field)
-      request.env['GEOIP'].send field
-    end
-  end
-  
-  get '/' do
-    haml :index
-  end
+use Rack::MMGeoip         # To stuff the MMGeoip proxy object into the env hash.
+helpers Sinatra::MMGeoip  # Only if you want to use the convinient #geoip helper method.
+
+MMGeoip.db_path = '/Users/niko/projects/mm_geoip/data/GeoLiteCity.dat'
+
+get '/' do
+  puts "A request from #{geoip.city} (#{geoip.country_name}) [MMGeoip v#{MMGeoip::VERSION}]"
+  haml :index
 end
 
 __END__
@@ -22,10 +16,9 @@ __END__
 @@ index
 %html
   %head
-    %title mm_geoip with Sinatra
+    %title mm_geoip with Sinatra, demonstrating usage in routes and views.
   %body
     %ul
       - (MMGeoip::FIELDS + [:region_name]).each do |field|
-        %li= "#{field}: #{geoip field}"
-
+        %li= "#{field}: #{geoip.send field}"
 
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 $LOAD_PATH << File.dirname(File.expand_path __FILE__)
 require 'app'
-run App
+
+run Sinatra::Application
